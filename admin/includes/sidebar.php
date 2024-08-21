a18bb1ac3bf15509f53c1a63205fdc4a33269d86
Sajed Saidi <programmingstudent8@gmail.com>
2024-08-21 17:18:11 +0300

    Add admin files

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,32 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.php">
+            <span class="align-middle">AdminKit</span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">Pages</li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="index.php">
+                    <i class="align-middle" data-feather="sliders"></i>
+                    <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-profile.php">
+                    <i class="align-middle" data-feather="user"></i>
+                    <span class="align-middle">Profile</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="template.php">
+                    <i class="align-middle" data-feather="log-in"></i>
+                    <span class="align-middle">Template</span>
+                </a>
+            </li>
+
+    </div>
+</nav>
